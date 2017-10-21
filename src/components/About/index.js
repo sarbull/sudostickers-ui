f095f22c14a480269a907479f5b2ced3f6c3f3a7
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-10-21 23:30:33 +0300

    Update about us page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,12 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h2>About</h2>
-        <ul>
-          <li>Cezar Sirbu</li>
-          <li>Popescu Florin</li>
-        </ul>
+        <h2>About Us</h2>
+        <p>We are Sudo Stickers! We think we have something in common, passion for stickers and for programming.</p>
+
+        <p>We decided to combine these two passions.</p>
+
+        <p>Sincerely, <br/> The Sudo Sticker Team</p>
       </div>
     );
   }
